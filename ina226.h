0a495fb6b0a483a2bfe7d66387b10302870a4cf4
eldlthesecond-droid <eldl.thesecond@gmail.com>
2026-04-15 22:01:32 +0100

    Update ina226.h

--- ina226.h
+++ ina226.h
@@ -3,12 +3,27 @@
  * ----------------------------------
  * Simple, beginner-friendly driver for Texas Instruments INA226.
  *
- * IMPORTANT NOTICE:
- * - This code is provided "as-is" without any warranty.
- * - No guarantee of correctness, fitness for purpose, or safety.
- * - Use at your own risk.
+ * MIT License
+ * 
+ * Copyright (c) 2026 [Your Name or Organization]
  *
- * Copyright (c) 2026
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef INA226_H
@@ -17,7 +32,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#include "main.h"
+
+#include "stm32fxxx_hal.h"
 
 /* ---------------- I2C ADDRESS ---------------- */
 #define INA226_ADDR (0x40 << 1)   // default 7-bit address shifted for STM32 HAL
@@ -33,12 +49,9 @@ extern "C" {
 #define INA226_REG_ALERT      0x07
 
 /* ---------------- FUNCTION PROTOTYPES ---------------- */
-
-/* Low-level I2C access */
 HAL_StatusTypeDef INA226_WriteRegister(uint8_t reg, uint16_t value);
 HAL_StatusTypeDef INA226_ReadRegister(uint8_t reg, uint16_t *value);
 
-/* Setup */
 void INA226_Init(void);
 void INA226_SetConfig(uint16_t config);
 void INA226_SetCalibration(uint16_t calib);
